30290448575a97dd2eece2cdd4d6d37f1a513316
DefNeoRoman <d89056411060@yandex.ru>
2017-08-31 15:00:09 +0300

    update and delete url was fixed

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -4,10 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
 @RequestMapping(value= "/meals")
@@ -19,6 +27,43 @@ public class JspMealController extends MealRestController{
     public String mealList(Model model){
         model.addAttribute("meals",super.getAll());
         return "meals";
+    }
+    @RequestMapping(value = "/delete",method = RequestMethod.GET)
+    public String delete(HttpServletRequest request){
+        super.delete(getId(request));
+        return "redirect:/meals";
+    }
+    @RequestMapping(value = "/update", method = RequestMethod.GET)
+    public String update(HttpServletRequest request, Model model){
+        model.addAttribute("meal",super.get(getId(request)));
+        return "mealForm";
+    }
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String create(Model model){
+        model.addAttribute("meal",new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS),"",2000));
+        return "mealForm";
+    }
+    @RequestMapping(method = RequestMethod.POST)
+    public String updateOrCreate(HttpServletRequest request){
+        Meal meal = new Meal(LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.valueOf(request.getParameter("calories")));
+
+        if (request.getParameter("id").isEmpty()){
+            super.create(meal);
+        }else{
+            super.update(meal,getId(request));
+        }
+        return "redirect:meals";
+    }
+    @RequestMapping(value = "/filter", method = RequestMethod.POST)
+    public String getBetween(HttpServletRequest request, Model model){
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+        model.addAttribute("meals",super.getBetween(startDate,startTime,endDate,endTime));
+        return "/meals";
     }
     private int getId(HttpServletRequest request){
         String parameterId = Objects.requireNonNull(request.getParameter("id"));
--- src/main/webapp/WEB-INF/jsp/fragments/headTag.jsp
+++ src/main/webapp/WEB-INF/jsp/fragments/headTag.jsp
@@ -2,8 +2,11 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jstl/core" %>
 
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title><spring:message code="app.title"/></title>
-    <link rel="stylesheet" href="resources/css/style.css">
+    <title>CaloriesManager</title>
+    <c:set var="url">${pageContext.request.requestURL}</c:set>
+    <base href="${fn:substring(url, 0, fn:length(url) - fn:length(pageContext.request.requestURI))}${pageContext.request.contextPath}/" />
+    <link rel="stylesheet" href="/resources/css/style.css">
 </head>
