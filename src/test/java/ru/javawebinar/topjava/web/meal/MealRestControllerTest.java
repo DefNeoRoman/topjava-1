6587c4c401fac87755fd20d4a6fda0b1c6da1c8c
DefNeoRoman <d89056411060@yandex.ru>
2017-09-22 17:01:46 +0300

    add testUnauthorized

--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -104,4 +104,10 @@ public class MealRestControllerTest extends AbstractControllerTest {
                 .andExpect(content().json(JsonUtil.writeValue(
                         MealsUtil.getWithExceeded(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1), USER.getCaloriesPerDay()))));
     }
+    @Test
+    public void testUnauthorized() throws Exception {
+        mockMvc.perform(get(REST_URL + MEAL1_ID))
+                .andExpect(status().is(401));
+    }
+
 }
